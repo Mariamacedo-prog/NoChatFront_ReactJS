26aa768fc0f8fac3b059fe683cc3fedc4dd96152
unknown <mariamacedo2xx@gmail.com>
2021-09-24 12:01:22 -0300

    Iniciating to perform NoChat-Front with Auth saving in Cookies

---
+++ src/helpers/Auth.ts
@@ -0,0 +1,15 @@
+import Cookies from "js-cookie";
+
+export const isLogged = () => {
+  let token = Cookies.get("token");
+
+  return token ? true : false;
+};
+
+export const doLogin = (token: string) => {
+  Cookies.set("token", token, { expires: 999 });
+};
+
+export const doLogout = () => {
+  Cookies.remove("token");
+};
